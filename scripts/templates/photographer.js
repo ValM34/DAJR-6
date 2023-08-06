125fac5d993eb8e8f157876081d2a41006baf6e4
valentin moreau <valentin.moreau34750@gmail.com>
2023-08-06 15:13:56 +0200

    home page integration

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,42 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    console.log(data);
+    const { name, portrait, price, tagline, id, country, city } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const linkContainer = document.createElement('a');
+        linkContainer.setAttribute("href", name);
+        linkContainer.setAttribute("alt", name);
+        article.appendChild(linkContainer);
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        linkContainer.appendChild(img);
+        linkContainer.appendChild(h2);
+
+        // div infos container
+        const infosContainer = document.createElement('div');
+        infosContainer.classList.add('photographer-card-description');
+        article.appendChild(infosContainer);
+
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+        const taglineEl = document.createElement( 'p' );
+        taglineEl.textContent = tagline;
+        const priceEl = document.createElement( 'p' );
+        priceEl.textContent = `${price}€/jour`;
+
+        infosContainer.appendChild(location);
+        infosContainer.appendChild(taglineEl);
+        infosContainer.appendChild(priceEl);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
